abc23585d2db81cbef86da985707de59fdb49929
gMartins <45578391+psytins@users.noreply.github.com>
2022-05-20 20:39:24 +0100

    add register page

--- views/view.py
+++ views/view.py
@@ -1,9 +1,8 @@
-from cProfile import label
 from tkinter import *
-#Global
-WIDTH, HEIGHT = 900,900
 
-def login_page(root):
+def login_page():
+    root = Tk()
+    root.title("Show Time! - Login")
     root.geometry("350x350")
     root.resizable(False,False)
     Label(root, text="Welcome to Show Time!", padx=10,pady=10).grid(column=0,row=0, sticky=W)
@@ -15,15 +14,33 @@ def login_page(root):
     Entry(root,width=30,borderwidth=2).grid(column=1,row=2)
     #Button
     Button(root,text="Login",width=10,height=2).grid(column=0,row=3)
-    Button(root,text="Register",width=10,height=2).grid(column=1,row=3)
+    Button(root,text="Register",width=10,height=2,command=register_page).grid(column=1,row=3)
+    root.mainloop()
+
+def register_page(): 
+    root = Tk()
+    root.title("Show Time! - Register")
+    root.geometry("350x500")
+    root.resizable(False,False)
+    Label(root, text="Welcome to Show Time!", padx=10,pady=10).grid(column=0,row=0, sticky=W)
+    Label(root, text="Please register", padx=5,pady=6).grid(column=0,row=1, sticky=W)
+    #Full Name
+    Label(root, text="Full Name", padx=10,pady=10).grid(column=0,row=2, sticky=W)
+    Entry(root,width=30,borderwidth=2).grid(column=1,row=2)
+    #Email
+    Label(root, text="Email", padx=10,pady=10).grid(column=0,row=3, sticky=W)
+    Entry(root,width=30,borderwidth=2).grid(column=1,row=3)
+    #Password
+    Label(root, text="Password", padx=10,pady=10).grid(column=0,row=4, sticky=W)
+    Entry(root,width=30,borderwidth=2).grid(column=1,row=4)
+    #Button
+    Button(root,text="Register",width=10,height=2).grid(column=0,row=5)
+    Button(root,text="Login",width=10,height=2,command=login_page).grid(column=1,row=5)
     root.mainloop()
 
 def main():
-    root = Tk()
-    root.title("Show Time!")
-    
-
-    login_page(root)
+    login_page()
+    register_page()
     
 
     
