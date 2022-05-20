ba2059777a44d2602f9cfbb80d8dbf3d464d9113
gMartins <45578391+psytins@users.noreply.github.com>
2022-05-20 20:24:32 +0100

    add seat type parameter in reservation class

--- models/reservation.py
+++ models/reservation.py
@@ -1,7 +1,8 @@
 class Reservation(object):
-    def __init__(self, id : int, user, show, price : float, seat : str ) -> None:
+    def __init__(self, id : int, user, show, price : float, seat : str, type : str ) -> None:
         self.ID = id
         self.UserObj = user
         self.ShowObjt = show
         self.Price = price
         self.SeatNumber = seat
+        self.Type = type
