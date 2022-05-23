acb2e028290f34f0e19c9dd89544a1319570b45e
gMartins <45578391+psytins@users.noreply.github.com>
2022-05-23 11:01:10 +0100

    add get functions

--- models/user.py
+++ models/user.py
@@ -1,13 +1,22 @@
 class User(object):
     def __init__(self, id : int, name : str, email : str, password : str, is_admin : bool) -> None:
-        self.ID = id
-        self.FullName = name
-        self.Email = email
-        self.Password = password
-        self.Admin = is_admin
+        self.id = id
+        self.fullname = name
+        self.email = email
+        self.password = password
+        self.admin = is_admin
     
     def getID(self):
-        return self.ID
+        return self.id
     
     def getFullName(self):
-        return self.FullName
+        return self.fullname
+
+    def getEmail(self):
+        return self.email
+
+    def getPassword(self):
+        return self.password
+
+    def isAdmin(self):
+        return self.admin
