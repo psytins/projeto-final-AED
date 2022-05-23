d9fc1cef4714dd972dd1da6a48b006347bf8e06e
joaop3152 <joaop3152@gmail.com>
2022-05-23 11:20:38 +0100

    add get functions on reservation

--- models/reservation.py
+++ models/reservation.py
@@ -1,8 +1,34 @@
 class Reservation(object):
-    def __init__(self, id : int, user, show, price : float, seat : str, type : str ) -> None:
-        self.ID = id
-        self.UserObj = user
-        self.ShowObjt = show
-        self.Price = price
-        self.SeatNumber = seat
-        self.Type = type
+    def __init__(self, id : int, user_id : int, user_name : str, show_id : int, show_name : str, price : float, seat : str, type : str ) -> None:
+        self.id = id
+        self.user_id = user_id
+        self.user_name = user_name
+        self.show_id = show_id
+        self.show_name = show_name
+        self.price = price
+        self.seat_number = seat
+        self.type = type
+
+    def getID(self):
+        return self.id
+
+    def getUserID(self):
+        return self.user_id
+
+    def getUserName(self):
+        return self.user_name
+
+    def GetShowID(self):
+        return self.show_id
+
+    def GetName(self):
+        return self.show_name
+
+    def GetPrice(self):
+        return self.price
+
+    def GetSeatNumber(self):
+        return self.seat_number
+
+    def GetType(self):
+        return self.type
