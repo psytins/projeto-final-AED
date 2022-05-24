a6dedf760100692528489cac9156ef4fb9f109ac
gMartins <45578391+psytins@users.noreply.github.com>
2022-05-24 19:23:32 +0100

    add save function to save data to json files

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,7 +3,8 @@ import models.show as show
 import models.user as user
 import models.model as model
 import json
-
+#Função utilizada quando se dá o início do programa
+#Esta função vai carrregar todos os dados dos JSON nas variáveis globais do tipo lista (ver no models/model)
 def start():
     #Associar a listas globais para cada objeto
     model.User_List = list()
@@ -33,7 +34,8 @@ def start():
         model.Reservation_List.append(temp_reservations)
         temp_reservations = None
     f_reservations.close()
-
+#Função utilizada quando se termina o programa
+#Esta função vai carrregar todos os dados das listas globais (model) nos ficheiros JSON
 def save():
     model.User_List = list()        #temp
     model.Reservation_List = list() #temp
@@ -44,7 +46,7 @@ def save():
     # dump para tranformar num objecto json
     # file write
     # --------------
-    #For users
+    #Save in JSON - For users
     user_json = list()
     for users in model.User_List:
         temp_dict_users = dict()
@@ -56,7 +58,42 @@ def save():
         user_json.append(temp_dict_users)
     load_json = json.dumps(user_json)
     #Write in json file
-    with open("./data/test_user.json","w") as file:
+    with open("./data/test_user.json","w") as file: # TEMPORARY FILE IS LOADED!
         file.write(load_json)
         file.close()
-        
+    #######################################################
+    #Save in JSON - For shows
+    show_json = list()
+    for shows in model.Show_List:
+        temp_dict_shows = dict()
+        temp_dict_shows["ID"] = shows.getID()
+        temp_dict_shows["Show Name"] = shows.getShowName()
+        temp_dict_shows["Date"] = shows.getDate()
+        temp_dict_shows["Capacity"] = shows.getCapacity() 
+        temp_dict_shows["Description"] = shows.getDescription()
+        temp_dict_shows["Room"] = shows.getRoom()
+        show_json.append(temp_dict_shows)
+    load_json = json.dumps(show_json)
+    #Write in json file
+    with open("./data/test_show.json","w") as file: # TEMPORARY FILE IS LOADED!
+        file.write(load_json)
+        file.close()       
+    #######################################################
+    #Save in JSON - For reservation
+    reservation_json = list()
+    for reservations in model.Reservation_List:
+        temp_dict_reservations = dict()
+        temp_dict_reservations["ID"] = reservations.getID()
+        temp_dict_reservations["User ID"] = reservations.getUserID()
+        temp_dict_reservations["User Name"] = reservations.getUserName()
+        temp_dict_reservations["Show ID"] = reservations.getShowID()
+        temp_dict_reservations["Show Name"] = reservations.getShowName()
+        temp_dict_reservations["Type"] = reservations.SeatType()
+        temp_dict_reservations["Price"] = reservations.getPrice()
+        temp_dict_reservations["Seat Number"] = reservations.getSeatNumber()
+        reservation_json.append(temp_dict_reservations)
+    load_json = json.dumps(reservation_json)
+    #Write in json file
+    with open("./data/test_reservation.json","w") as file: # TEMPORARY FILE IS LOADED!
+        file.write(load_json)
+        file.close()  
