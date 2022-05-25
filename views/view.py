47fbb1538dd3ce6c73acf8d1d66576f2b0cf269f
ongserra <mafalda@pedro-serra.com>
2022-05-25 03:30:16 +0100

    add user area2

--- views/view.py
+++ views/view.py
@@ -1,6 +1,8 @@
+from operator import ne
 from tkinter import *
 from turtle import st
 import controllers.controller as controller
+#from PIL import ImageTk,Image
 
 def login_page(parent=None):
     if(parent is not None):
@@ -51,7 +53,7 @@ def user_area():
     root.resizable(False,False)
     #texto
     Label(root, text="Os seus Bilhetes", font=("Arial", 14), background="gray").grid(row=0, column=0)
-    #separaçao
+    #separaçao linha
     Label(root, text=" ").grid(row=1, column=0)
     #info dos bilhetes
     Label(root, text="Nome do Espetáculo:", font=("Arial", 9), justify="right").grid(row=2, column=0, sticky=W)
@@ -59,10 +61,23 @@ def user_area():
     Label(root, text="Tipo de bilhete:", font=("Arial", 9), justify="right").grid(row=4, column=0, sticky=W)
     Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=5, column=0, sticky=W)
     Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=6, column=0, sticky=W)
+    #separacoes linhas
+    Label(root, text=" ").grid(row=7, column=0)
+    Label(root, text=" ").grid(row=8, column=0)
+    #info dos bilhetes 2
+    Label(root, text="Nome do Espetáculo:", font=("Arial", 9), justify="right").grid(row=9, column=0, sticky=W)
+    Label(root, text="Data do Espetáculo:", font=("Arial", 9), justify="right").grid(row=10, column=0, sticky=W)
+    Label(root, text="Tipo de bilhete:", font=("Arial", 9), justify="right").grid(row=11, column=0, sticky=W)
+    Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=12, column=0, sticky=W)
+    Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=13, column=0, sticky=W)
+    #imagem perfil VER COMO FZR
+    #ImageTk(root, file="perfil.ppm").grid(row=0, column= 10, sticky=E)
+    #nome do user
+    Label(root, text="User:", font=("Arial", 9), justify="right").grid(row=6, column=30, sticky=NE)
+    #botao logout
+    Button(root,text="Log Out",width=10,height=2, justify="right").grid(column=20,row=7, sticky=NE)
     root.mainloop()
 
 def main():
     #login_page()
     user_area()
-
-    
