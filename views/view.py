303235e23221a2d71f6f6f8dd6c7c8c3e6892067
Mafalda Serra <30008555@students.ual.pt>
2022-05-25 11:38:52 +0100

    add user area 3

--- views/view.py
+++ views/view.py
@@ -46,6 +46,7 @@ def register_page(parent):
     root.mainloop()
 
 def user_area():
+    
     #parent.destroy()
     root = Tk()
     root.title("Show Time! - Área do utilizador")
@@ -59,23 +60,36 @@ def user_area():
     Label(root, text="Nome do Espetáculo:", font=("Arial", 9), justify="right").grid(row=2, column=0, sticky=W)
     Label(root, text="Data do Espetáculo:", font=("Arial", 9), justify="right").grid(row=3, column=0, sticky=W)
     Label(root, text="Tipo de bilhete:", font=("Arial", 9), justify="right").grid(row=4, column=0, sticky=W)
-    Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=5, column=0, sticky=W)
-    Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=6, column=0, sticky=W)
+    Label(root, text="Número de Bilhetes:", font=("Arial", 9), justify="right").grid(row=5, column=0, sticky=W)
+    Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=6, column=0, sticky=W)
+    Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=7, column=0, sticky=W)
     #separacoes linhas
-    Label(root, text=" ").grid(row=7, column=0)
     Label(root, text=" ").grid(row=8, column=0)
+    Label(root, text=" ").grid(row=9, column=0)
     #info dos bilhetes 2
-    Label(root, text="Nome do Espetáculo:", font=("Arial", 9), justify="right").grid(row=9, column=0, sticky=W)
-    Label(root, text="Data do Espetáculo:", font=("Arial", 9), justify="right").grid(row=10, column=0, sticky=W)
-    Label(root, text="Tipo de bilhete:", font=("Arial", 9), justify="right").grid(row=11, column=0, sticky=W)
-    Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=12, column=0, sticky=W)
-    Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=13, column=0, sticky=W)
+    Label(root, text="Nome do Espetáculo:", font=("Arial", 9), justify="right").grid(row=10, column=0, sticky=W)
+    Label(root, text="Data do Espetáculo:", font=("Arial", 9), justify="right").grid(row=11, column=0, sticky=W)
+    Label(root, text="Tipo de bilhete:", font=("Arial", 9), justify="right").grid(row=12, column=0, sticky=W)
+    Label(root, text="Número de Bilhetes:", font=("Arial", 9), justify="right").grid(row=13, column=0, sticky=W)
+    Label(root, text="Lugar:", font=("Arial", 9), justify="right").grid(row=14, column=0, sticky=W)
+    Label(root, text="Preço do Bilhete:", font=("Arial", 9), justify="right").grid(row=15, column=0, sticky=W)
+    #separacoes colunas
+    Label(root, text="            ").grid(row=0, column=1)
+    Label(root, text="            ").grid(row=1, column=2)
+    Label(root, text="            ").grid(row=2, column=3)
+    Label(root, text="            ").grid(row=3, column=4)
+    Label(root, text="            ").grid(row=4, column=5)
+    Label(root, text="            ").grid(row=5, column=6)
+    Label(root, text="            ").grid(row=6, column=7)
+    Label(root, text="            ").grid(row=7, column=8)
+    Label(root, text="            ").grid(row=8, column=9)
+    #nome do user
+    Label(root, text="User:", font=("Arial", 9), justify="right").grid(row=8, column=9, sticky=NE)
     #imagem perfil VER COMO FZR
     #ImageTk(root, file="perfil.ppm").grid(row=0, column= 10, sticky=E)
-    #nome do user
-    Label(root, text="User:", font=("Arial", 9), justify="right").grid(row=6, column=30, sticky=NE)
+    #perfil = PhotoImage(file="./data/perfil.ppm") // , image=perfil,
     #botao logout
-    Button(root,text="Log Out",width=10,height=2, justify="right").grid(column=20,row=7, sticky=NE)
+    Button(root,text="Log Out",width=10,height=2, font=("Arial", 9, "bold"), justify="right", background="red").grid(column=20,row=9, sticky=NE)
     root.mainloop()
 
 def main():
