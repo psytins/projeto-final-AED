ce9fdd272fd5c04eb1e3d9bafd13e473d7c41988
gMartins <45578391+psytins@users.noreply.github.com>
2022-06-04 15:56:14 +0100

    login, registrate and user space working

--- views/view.py
+++ views/view.py
@@ -1,13 +1,26 @@
-from operator import ne
 from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk,Image
-from turtle import st
-import tkinter as tk
 import controllers.controller as controller
 import models.model as model
 
 background="#a6c8e0"
+
+def autenticate_user(parent,email,password):
+    auth = controller.authenticate_user(email,password)
+    if(auth == 0): 
+        Label(parent, text="O utilizador não existe", padx=10,pady=10).grid(column=0,row=4, sticky=W)
+    elif(auth == -1):
+        Label(parent, text="A palavra passe está incorreta", padx=10,pady=10).grid(column=0,row=4, sticky=W)
+    else:
+        user_area(parent,auth)
+
+def registrate_user(parent,email,name,password):
+    control = controller.registrate_user(name,email,password)
+    if(control == 0): 
+        Label(parent, text="O email já existe", padx=10,pady=10).grid(column=0,row=6, sticky=W)
+    else:
+        user_area(parent,control)
 
 def login_page(parent=None):
     if(parent is not None):
@@ -18,13 +31,15 @@ def login_page(parent=None):
     root.resizable(False,False)
     Label(root, text="Welcome to Show Time!", padx=10,pady=10).grid(column=0,row=0, sticky=W)
     #Email
+    user_email = StringVar()
     Label(root, text="Email", padx=10,pady=10).grid(column=0,row=1, sticky=W)
-    Entry(root,width=30,borderwidth=2).grid(column=1,row=1)
+    Entry(root,width=30,borderwidth=2,textvariable=user_email).grid(column=1,row=1)
     #Password
+    user_password = StringVar()
     Label(root, text="Password", padx=10,pady=10).grid(column=0,row=2, sticky=W)
-    Entry(root,width=30,borderwidth=2, show="*").grid(column=1,row=2)
+    Entry(root,width=30,borderwidth=2, textvariable=user_password, show="*").grid(column=1,row=2)
     #Button
-    Button(root,text="Login",width=10,height=2).grid(column=0,row=3)
+    Button(root,text="Login",width=10,height=2,command=lambda:autenticate_user(root,user_email.get(),user_password.get())).grid(column=0,row=3)
     Button(root,text="Register",width=10,height=2,command=lambda:register_page(root)).grid(column=1,row=3)
     root.mainloop()
 
@@ -37,49 +52,71 @@ def register_page(parent):
     Label(root, text="Welcome to Show Time!", padx=10,pady=10).grid(column=0,row=0, sticky=W)
     Label(root, text="Please register", padx=5,pady=6).grid(column=0,row=1, sticky=W)
     #Full Name
+    regist_name = StringVar()
     Label(root, text="Full Name", padx=10,pady=10).grid(column=0,row=2, sticky=W)
-    Entry(root,width=30,borderwidth=2).grid(column=1,row=2)
+    Entry(root,width=30,borderwidth=2,textvariable=regist_name).grid(column=1,row=2)
     #Email
+    regist_email = StringVar()
     Label(root, text="Email", padx=10,pady=10).grid(column=0,row=3, sticky=W)
-    Entry(root,width=30,borderwidth=2).grid(column=1,row=3)
+    Entry(root,width=30,borderwidth=2,textvariable=regist_email).grid(column=1,row=3)
     #Password
+    regist_password = StringVar()
     Label(root, text="Password", padx=10,pady=10).grid(column=0,row=4, sticky=W)
-    Entry(root,width=30,borderwidth=2, show="*").grid(column=1,row=4)
+    Entry(root,width=30,borderwidth=2,textvariable=regist_password,show="*").grid(column=1,row=4)
     #Button
-    Button(root,text="Register",width=10,height=2).grid(column=0,row=5)
+    Button(root,text="Register",width=10,height=2,command=lambda:registrate_user(root,regist_email.get(),regist_name.get(),regist_password.get())).grid(column=0,row=5)
     Button(root,text="Login",width=10,height=2,command=lambda:login_page(root)).grid(column=1,row=5)
     root.mainloop()
 
-def user_area():
+def user_area(parent,session):
+    parent.destroy()
     root = Tk()
-    root.title("Show Time! - Área do utilizador")
+    root.title(f"Show Time! - Área do utilizador - {session.getFullName()}")
     root.geometry("700x500")
     root.resizable(False,False)
     root.config(bg=background)
-    
     #texto
-    Label(root, text="Informações", font=("Verdana", 16), background="#7eb6de").grid(row=0, column=0, sticky=W)
-
+    Label(root, text="Escolha o bilhete", font=("Verdana", 16), background="#7eb6de").grid(row=0, column=0, sticky=W)
     #dropdown 
-
+    #Get shows 
+    SHOW = list()
+    for reservations in controller.model.Reservation_List:
+        if(reservations.getUserID() == session.getID()):
+            SHOW.append(f"#{reservations.getID()} - {reservations.getShowName()} [{reservations.getSeatNumber()}]")
+    options = StringVar(root)
+    if(len(SHOW) == 0): 
+        options.set("None")
+        option_menu = OptionMenu(root, options,"None")
+    else: 
+        options.set(SHOW[0]) # default value
+        option_menu = OptionMenu(root, options, *SHOW)
+    option_menu.grid(row=1,column=0,sticky=W)
 
     #pop up da info dos bilhetes
     def clicker():
-        global pop
-        pop = Toplevel(root)
-        pop.title("Informações!")
-        pop.geometry("400x200")
-        pop.config(bg=background)
-        
-        pop_label1 = Label(pop, text="Nome do Espetáculo:", font=("Arial", 9), bg=background).grid(row=0, column=0, sticky=W)
-        pop_label2 = Label(pop, text="Data do Espetáculo:", font=("Arial", 9), bg=background).grid(row=1, column=0, sticky=W)
-        pop_label3 = Label(pop, text="Tipo de bilhete:", font=("Arial", 9), bg=background).grid(row=2, column=0, sticky=W)
-        pop_label4 = Label(pop, text="Número de Bilhetes:", font=("Arial", 9), bg=background).grid(row=3, column=0, sticky=W)
-        pop_label5 = Label(pop, text="Lugar:", font=("Arial", 9), bg=background).grid(row=4, column=0, sticky=W)
-        pop_label6 = Label(pop, text="Preço do Bilhete:", font=("Arial", 9), bg=background).grid(row=5, column=0, sticky=W)
-        pop_label7 = Label(pop, text=" ", bg=background).grid(row=6, column=0)
-        cancel = Button(pop, text="Cancel", command = lambda: choice("cancel"), bg="gray")
-        cancel.grid(row=7, column=2)
+        if(options.get() != "None"):
+            global pop
+            pop = Toplevel(root) # Criar uma página em cima da página atual
+            reservation_id = options.get()[1:5]
+            curr_reserv = None # Reservation Object
+            curr_show = None #Show Object
+            for reservations in controller.model.Reservation_List:
+                if(reservations.getID() == reservation_id):
+                    curr_reserv = reservations
+            for shows in controller.model.Show_List:
+                if(shows.getID() == curr_reserv.getShowID()):
+                    curr_show = shows
+            pop.title(f"Informações - #{curr_reserv.getID()}")
+            pop.geometry("400x200")
+            pop.config(bg=background)
+            pop_label1 = Label(pop, text=f"Nome do Espetáculo: {curr_show.getShowName()}", font=("Arial", 9), bg=background).grid(row=0, column=0, sticky=W)
+            pop_label2 = Label(pop, text=f"Data do Espetáculo: {curr_show.getDate()}", font=("Arial", 9), bg=background).grid(row=1, column=0, sticky=W)
+            pop_label3 = Label(pop, text=f"Tipo de bilhete: {curr_show.getSeatType(curr_reserv.getSeatNumber())}", font=("Arial", 9), bg=background).grid(row=2, column=0, sticky=W)
+            pop_label5 = Label(pop, text=f"Lugar: {curr_reserv.getSeatNumber()}", font=("Arial", 9), bg=background).grid(row=3, column=0, sticky=W)
+            pop_label6 = Label(pop, text=f"Preço do Bilhete: {curr_reserv.getPrice()}", font=("Arial", 9), bg=background).grid(row=4, column=0, sticky=W)
+            pop_label7 = Label(pop, text=" ", bg=background).grid(row=5, column=0)
+            cancel = Button(pop, text="Cancel", command = lambda: choice("cancel"), bg="gray")
+            cancel.grid(row=6, column=2)
 
     def choice(option):
         pop.destroy()
@@ -88,41 +125,18 @@ def user_area():
     #botao na area do user q mostra os bilhetes
     #porque é que no command = dropdown?
     #alterei o dropdown para clicker
-    Button(root, text="Bilhetes", command=clicker, width=30, height=3, font=("Arial", 11, "bold"), bg="#3d9adb").grid(column=0, row=3)
+    Button(root, text="Ver Bilhete", command=clicker, width=30, height=3, font=("Arial", 11, "bold"), bg="#3d9adb").grid(column=0, row=2)
     
-    #separacoes linhas
-    Label(root, text=" ", bg=background).grid(row=8, column=0)
-    Label(root, text=" ", bg=background).grid(row=9, column=0)
-    
-    #separacoes colunas !!!!!!!!POR FAVOR ARRANJAR OUTRA FORMA DE FAZER A SEPARACAO COM COLUNAS !!!!!!!!!!!
-    Label(root, text="                                                               ", bg=background).grid(row=0, column=3)
-    Label(root, text="                                                               ", bg=background).grid(row=1, column=3)
-    Label(root, text="                                                               ", bg=background).grid(row=2, column=3)
-    Label(root, text="                                                               ", bg=background).grid(row=3, column=3)
-    Label(root, text="                                                               ", bg=background).grid(row=4, column=3)
-    Label(root, text="                                                                                              ", bg=background).grid(row=5, column=3)
     #nome do user
-    Label(root, text="User:", font=("Arial", 9), justify="right", bg=background).grid(row=4, column=9, sticky=NE)
+    Label(root, text=f"Bem-Vindo: {session.getFullName()}", font=("Arial", 9), justify="right", bg=background).grid(row=4, column=0, sticky=W)
     #imagem teatro
     #my_img = ImageTk.PhotoImage(Image.open("./data/espetaculos.jpg"))
     #my_label = Label(root, image=my_img, height=250, width=350).grid(row=5, column=0)
     #botao logout
-    Button(root,text="Log Out",width=10,height=2, font=("Arial", 9, "bold"), background="red", command=root.quit).grid(column=20,row=5)
-    #separacoes das linhas
-    Label(root, text=" ", bg=background).grid(row=6, column=0)
-    Label(root, text=" ", bg=background).grid(row=7, column=0)
-    Label(root, text=" ", bg=background).grid(row=8, column=0)
-    Label(root, text=" ", bg=background).grid(row=9, column=0)
-    Label(root, text=" ", bg=background).grid(row=10, column=0)
-    Label(root, text=" ", bg=background).grid(row=11, column=0)
-    Label(root, text=" ", bg=background).grid(row=12, column=0)
-    Label(root, text=" ", bg=background).grid(row=13, column=0)
-    Label(root, text=" ", bg=background).grid(row=14, column=0)
-    Label(root, text=" ", bg=background).grid(row=15, column=0)
-    Label(root, text=" ", bg=background).grid(row=16, column=0)
-    
+    Button(root,text="Log Out",width=10,height=2, font=("Arial", 9, "bold"), background="red", command=lambda:login_page(root)).grid(column=0,row=5, sticky=W)
+
     #botao pagina principal
-    Button(root, text="Página Principal", width=12, height=2, bg="#50616e", command=root.quit).grid(row=14, column=20)
+    Button(root, text="Ver Espetáculos", width=12, height=2, bg="#50616e", command=root.quit).grid(row=6, column=0,sticky=W)
 
     root.mainloop()
 
@@ -233,4 +247,5 @@ def temp_menu():
             
 
 def main():
-    temp_menu()
+    controller.start()
+    login_page()
